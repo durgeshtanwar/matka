0ce172dc3014641c270226fb6d82bf6c858d5063
Durgesh <tanwardurgesh@gmail.com>
2020-08-27 11:49:30 +0530

    Added the add user feature

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -14,4 +14,47 @@ class Dashboard extends CI_Controller {
 		}
 	
 	}
+
+	public function newUser(){
+		
+		$this->form_validation->set_rules('username', 'Username', 'required|min_length[5]|callback_username_check');
+		$this->form_validation->set_rules('password', 'Password', 'required');
+		if ($this->form_validation->run() == FALSE)
+		{
+			$this->load->view('adminpanel/dashboard');
+		}
+		else {
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+			$address = $_POST['address'];
+			$phone = $_POST['phone'];
+			$status = $_POST['status'];	
+
+			$data = array(
+				'user_name' => $username,
+				'password' => $password,
+				'address' => $address,
+				'phone'	=> $phone,
+				'status' => $status
+			);
+
+			$result = $this->db->insert('users', $data);
+			$this->set_flashdata('item','Data inserted successfully');
+			$this->load->view('adminpanel/dashboard');
+		
+		}
+
+	}
+
+	public function username_check($str){
+
+		$query = $this->db->query("SELECT `userid` FROM `users` WHERE `user_name`= '$str'" );
+		if($query->num_rows()){
+			$this->form_validation->set_message('username_check', 'The {field} is already selected please use different username');
+                        return FALSE;
+		}
+		else {
+			return TRUE;
+		}
+	}
 }
